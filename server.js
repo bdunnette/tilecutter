8bc4348043c839367424f534332fc0a65c347704
Brian Dunnette <dunn0172@umn.edu>
2015-07-10 17:51:25 -0500

    tidying

--- server.js
+++ server.js
@@ -8,87 +8,91 @@ var levelup = require('level');
 var Jobs = require('level-jobs');
 
 var server = new Hapi.Server();
-server.connection({ port: 3030 });
+server.connection({
+  port: 3030
+});
 
 var db = levelup('.jobs');
 
-function worker(payload, cb) {
+function worker(payload, done) {
   server.log('info', "Processing file:" + payload.filename);
   sharp(path.join(__dirname, "slides", payload.filename)).metadata(function(err, metadata) {
     console.log(err);
     console.log(metadata);
   });
+  done();
 }
 
 var maxConcurrency = 2;
 var queue = Jobs(db, worker, maxConcurrency);
 
 server.route({
-    method: 'POST',
-    path: '/submit',
-    config: {
-	validate: {
-          payload: {
-            file: Joi.object().required()
-    	}},
-        payload: {
-	    maxBytes: 2147483648,
-            output: 'stream',
-            parse: true,
-            allow: 'multipart/form-data'
-        },
+  method: 'POST',
+  path: '/submit',
+  config: {
+    validate: {
+      payload: {
+        file: Joi.object().required()
+      }
+    },
+    payload: {
+      maxBytes: 2147483648,
+      output: 'stream',
+      parse: true,
+      allow: 'multipart/form-data'
+    },
 
-        handler: function (request, reply) {
-            var data = request.payload;
-            if (data.file) {
-                var name = data.file.hapi.filename;
-		console.log(name);
-		console.log(__dirname);
-                var filePath = path.join(__dirname, "slides", name);
-                var file = fs.createWriteStream(filePath);
+    handler: function(request, reply) {
+      var data = request.payload;
+      if (data.file) {
+        var name = data.file.hapi.filename;
+        var filePath = path.join(__dirname, "slides", name);
+        var file = fs.createWriteStream(filePath);
 
-                file.on('error', function (err) {
-                    console.error(err)
-                });
+        file.on('error', function(err) {
+          console.error(err)
+        });
 
-                data.file.pipe(file);
+        data.file.pipe(file);
 
-                data.file.on('end', function (err) {
-                    var ret = {
-                        filename: data.file.hapi.filename,
-                        headers: data.file.hapi.headers
-                    }
-		    var payload = {filename: data.file.hapi.filename};
+        data.file.on('end', function(err) {
+          var ret = {
+            filename: data.file.hapi.filename,
+            headers: data.file.hapi.headers
+          }
+          var payload = {
+            filename: data.file.hapi.filename
+          };
 
-		    var jobId = queue.push(payload, function(err) {
-		      if (err) console.error('Error pushing work into the queue', err.stack);
-		    });
+          var jobId = queue.push(payload, function(err) {
+            if (err) console.error('Error pushing work into the queue', err.stack);
+          });
 
-		    reply(JSON.stringify(ret));
-                })
-            }
+          reply(JSON.stringify(ret));
+        })
+      }
 
-        }
     }
+  }
 });
 
 server.register({
-    register: Good,
-    options: {
-        reporters: [{
-            reporter: require('good-console'),
-            events: {
-                response: '*',
-                log: '*'
-            }
-        }]
-    }
-}, function (err) {
-    if (err) {
-        throw err; // something bad happened loading the plugin
-    }
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        response: '*',
+        log: '*'
+      }
+    }]
+  }
+}, function(err) {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
 
-    server.start(function () {
-        server.log('info', 'Server running at: ' + server.info.uri);
-    });
+  server.start(function() {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
